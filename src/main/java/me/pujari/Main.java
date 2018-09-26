c2a139dc39b878dbfe79da0a6dd8fc88ec387ad8
PushkarrajPujari <pushkarraj@pujari.me>
2018-09-26 16:37:28 +0530

    The client doesnt work with localhost, working on that

--- src/main/java/me/pujari/Main.java
+++ src/main/java/me/pujari/Main.java
@@ -1,13 +1,30 @@
 package me.pujari;
 
 public class Main {
+  static {
+        //for localhost testing only
+        javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(
+                new javax.net.ssl.HostnameVerifier(){
 
+                    public boolean verify(String hostname,
+                                          javax.net.ssl.SSLSession sslSession) {
+                        if (hostname.equals("localhost")) {
+                            return true;
+                        }
+                        return false;
+                    }
+                });
+    }
     public static void main(String[] args) {
         try {
+            System.setProperty("javax.net.ssl.trustStore", "D:\\KeyStore\\SSL\\keystore.jks");
+            System.setProperty("javax.net.ssl.keyStorePassword", "changeit" );
             System.out.println("------------ X ------------");
             new ApacheClient("https://google.com").connect().getContent();
             System.out.println("------------ X ------------");
             new HttpsClient("https://google.com").connect().getContent();
+            System.out.println("------------ X ------------");
+            new HttpsClient("https://localhost:8443").connect().getContent();
         } catch (Exception e) {
             e.printStackTrace();
         }
